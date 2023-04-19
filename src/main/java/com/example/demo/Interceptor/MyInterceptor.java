497359e4756eb26a94a23d4d7651816b6a574624
Leo <2697886927@qq.com>
2023-04-19 09:45:44 +0800

    SpringBoot 功能拓展实现自动化配置拦截器

---
+++ src/main/java/com/example/demo/Interceptor/MyInterceptor.java
@@ -0,0 +1,35 @@
+package com.example.demo.Interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.handler.Handler;
+import java.util.Calendar;
+
+@Component
+public class MyInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String uri = request.getRequestURI();
+        Object loginUser = request.getSession().getAttribute("loginUser");
+        if(uri.startsWith("/admin")&& null == loginUser){
+            response.sendRedirect("/toLoginPage");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+//        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+        request.setAttribute("currentYear", Calendar.getInstance().get(Calendar.YEAR));
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
